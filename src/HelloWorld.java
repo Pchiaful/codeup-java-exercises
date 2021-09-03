0fc42f27e5b055a18f553e88b639d65c66d94f3a
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-03 10:48:09 -0500

    syntax exercise revised

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,35 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
         int myFavoriteNumber;
         myFavoriteNumber = 8;
-        System.out.println(myFavoriteNumber);
+        System.out.println(myFavoriteNumber);;
         String myString;
         myString = "My string!";
         System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+        System.out.println(Integer.MAX_VALUE);
+//        int daNumbah = 999999999999999;
+//        System.out.println(daNumbah);
     }
 }
