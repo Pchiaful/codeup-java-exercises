430b9e5d5b36b65d39c16762aade5252bab313cf
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-03 09:42:44 -0500

    syntax exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!");
+        int myFavoriteNumber;
+        myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString;
+        myString = "My string!";
+        System.out.println(myString);
+    }
+}
