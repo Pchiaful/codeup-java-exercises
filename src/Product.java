b5cf38aca4363e93532c9bbc1cd97eb22991e4a1
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-09-13 13:28:41 -0500

    Product class completed, Thanks Kyle and Sebastian

---
+++ src/Product.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class Product {
+
+    /* TODO: create a class called Product with two private properties of name and priceInCents.
+//    Add a public static method called, findAveragePrice, that takes in an array of products
+//    and finds the average price of all products. Add a main method to the class to test some
+//    values.
+// */
+    private String name;
+    private double priceInCents;
+
+    public Product(String productName, int price) {
+        this.name = productName;
+        this.priceInCents = price;
+    }
+
+    public static Product[] products = {
+            new Product("milk", 10),
+            new Product("eggs", 50),
+            new Product("cheese", 25)
+    };
+
+    public static double findAveragePrice (Product[] listOfProducts) {
+        double sum = 0;
+        for (Product product : listOfProducts) {
+            sum += product.priceInCents;
+        }
+        return sum/products.length;
+    }
+
+    public static String[] printNames (Product[] listOfNames) {
+        String[] names = new String[products.length];
+        for (int i = 0; i < products.length; i++) {
+            names[i] = listOfNames[i].name;
+        }
+        return names;
+    }
+
+    public static void printAllNames () {
+        for (Product product : products) {
+            System.out.println(product.getNames());
+        }
+    }
+    public String getNames() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("The average price of all of the products is $%.2f.", findAveragePrice(products));
+
+        System.out.println();
+
+        System.out.println(Arrays.toString(printNames(products)));
+
+        printAllNames();
+    }
+}
--- src/TestCode.java
+++ src/TestCode.java
@@ -1,27 +1,35 @@
-import java.util.Scanner;
-public class TestCode {
-    public static void main(String[] args) {
-//        TODO: create an array of doubles called numbers and assign five different values (try with and without an initializer).
-//        Find the sum of all five double values using an enhanced for loop.
-
-//        double[] numbers = {55, 66, 77, 88, 99};
-        double[] numbers = new double[5];
-        numbers[0] = 55;
-        numbers[1] = 66;
-        numbers[2] = 77;
-        numbers[3] = 88;
-        numbers[4] = 99;
-
-        double total = 0;
-        for (double number : numbers ) {
-            total += number;
-            System.out.println(total);
-        }
-    }
-//    public class TernaryTester {
-//     public static void main(String[] args) {
-//                     int x = 5;
-//                     System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
-//                 }
-}
-
+//import java.util.Scanner;
+//public class TestCode {
+//    public static void main(String[] args) {
+////        TODO: create an array of doubles called numbers and assign five different values (try with and without an initializer).
+////        Find the sum of all five double values using an enhanced for loop.
+//
+////        double[] numbers = {55, 66, 77, 88, 99};
+////        double[] numbers = new double[5];
+////        numbers[0] = 55;
+////        numbers[1] = 66;
+////        numbers[2] = 77;
+////        numbers[3] = 88;
+////        numbers[4] = 99;
+////
+////        double total = 0;
+////        for (double number : numbers ) {
+////            total += number;
+////            System.out.println(total);
+////        }
+////    }
+////    public class TernaryTester {
+////     public static void main(String[] args) {
+////                     int x = 5;
+////                     System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+////                 }
+//
+////        /* TODO: create a class called Product with two private properties of name and priceInCents.
+////    Add a public static method called, findAveragePrice, that takes in an array of products
+////    and finds the average price of all products. Add a main method to the class to test some
+////    values.
+//// */
+//
+//
+//}
+//
