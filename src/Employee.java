1fc6254a64084dcbe7ef9c76cf2b48c427e2591b
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-10-01 23:47:03 -0500

    revisions

--- src/Employee.java
+++ src/Employee.java
@@ -3,7 +3,7 @@ public class Employee {
     public static String company = "Veridian Dynamics";
 
     public void displayEmployeeInfo() {
-        System.out.println(name + " works at " + company + ".");
+        System.out.println(name + " works for " + company + ".");
     }
 
     public void employeeQuote() {
--- src/Test.java
+++ src/Test.java
@@ -1,56 +1,55 @@
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//
-////        try{
-////            String testString = "This is a string";
-////            System.out.println(testString.length());
-////            System.out.println(testString.substring(177));
-////        } catch (IndexOutOfBoundsException something) {
-////            System.out.println("whoa dude out of bounds");
-////        }
-//
-//
-////        Scanner sc = new Scanner(System.in);
-////        System.out.println("Please enter an integer");
-////        String userInput = sc.nextLine();
-////        try {
-////            int userInputInteger = Integer.parseInt(userInput);
-////            System.out.println("The integer in your string is " + userInputInteger);
-////        } catch (NumberFormatException nfx) {
-////            System.out.println("Uh oh, " + nfx.getMessage() + " there is no integer!");
-////        } finally {
-////            System.out.println("Thank you for using this complex and useful app.");
-////        }
-//
-////        public void aMoreComplexAskForInteger(){
-////            Scanner scanner = new Scanner(System.in);
-////            System.out.println("Please enter an integer");
-////            String userInput = scanner.nextLine();
-//////            userInput = null;
-////            try {
-////                int userInputAsInteger = Integer.parseInt(userInput, 10, 15, 10);
-////            } catch (NullPointerException npx){
-////                System.out.println("Oops you didn't enter anything");
-////                npx.printStackTrace();
-////            } catch (IndexOutOfBoundsException iobx) {
-////                System.out.println("Oops your string wasn't long enough");
-////                iobx.printStackTrace();
-////            } catch (NumberFormatException nfx){
-////                System.out.println("No integer between chars 10 and 15");
-////                nfx.printStackTrace();
-////            } finally {
-////                System.out.println("That was useful wasn't it?");
-////            }
-////        }
-//
-//
-//
-//    }
-//
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+//        try{
+//            String testString = "This is a string";
+//            System.out.println(testString.length());
+//            System.out.println(testString.substring(177));
+//        } catch (IndexOutOfBoundsException something) {
+//            System.out.println("whoa dude out of bounds");
+//        }
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter an integer");
+//        String userInput = sc.nextLine();
+//        try {
+//            int userInputInteger = Integer.parseInt(userInput);
+//            System.out.println("The integer in your string is " + userInputInteger);
+//        } catch (NumberFormatException nfx) {
+//            System.out.println("Uh oh, " + nfx.getMessage() + " there is no integer!");
+//        } finally {
+//            System.out.println("Thank you for using this complex and useful app.");
+//        }
+
+//        public void aMoreComplexAskForInteger(){
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Please enter an integer");
+//            String userInput = scanner.nextLine();
+////            userInput = null;
+//            try {
+//                int userInputAsInteger = Integer.parseInt(userInput, 10, 15, 10);
+//            } catch (NullPointerException npx){
+//                System.out.println("Oops you didn't enter anything");
+//                npx.printStackTrace();
+//            } catch (IndexOutOfBoundsException iobx) {
+//                System.out.println("Oops your string wasn't long enough");
+//                iobx.printStackTrace();
+//            } catch (NumberFormatException nfx){
+//                System.out.println("No integer between chars 10 and 15");
+//                nfx.printStackTrace();
+//            } finally {
+//                System.out.println("That was useful wasn't it?");
+//            }
+//        }
+
+
+    }
+
 //        public void subListMaker(String sentence){
 //            Scanner scanner = new Scanner(System.in);
 //            System.out.println("Please enter an integer");
@@ -75,6 +74,6 @@
 //                System.out.println("thanks");
 //            }
 //            }
-//
-//
-//}
+
+
+}
